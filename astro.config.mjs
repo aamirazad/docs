2cbcbcc76bfbd378b4a45c5eebe39980060fceaf
Aamir Azad <aamirmazad@gmail.com>
2025-06-11 19:37:37 -0400

    nobara-pc: 2025-06-11 19:37:37

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { ion } from "starlight-ion-theme";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://louisescher.github.io",
+  site: "https://docs.aamira.me",
   integrations: [
     starlight({
       title: "Docs",
@@ -19,6 +19,16 @@ export default defineConfig({
           label: "[book] History Club",
           autogenerate: {
             directory: "history-club",
+          },
+        },
+      ],
+      head: [
+        {
+          tag: "script",
+          attrs: {
+            src: "https://cdn.jsdelivr.net/gh/litlyx/litlyx-js/browser/litlyx.js",
+            "data-project": "6849b51a09e0969e5bd76619",
+            defer: true,
           },
         },
       ],
